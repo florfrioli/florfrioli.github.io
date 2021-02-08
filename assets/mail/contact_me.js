f9129171580a6581ee8175510a7c57f8371da2b8
florfrioli <florenciaf@live.com.ar>
2021-02-07 22:29:50 -0300

    prueba form

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -1,12 +1,12 @@
-$(function () {
+$(function() {
     $(
         "#contactForm input,#contactForm textarea,#contactForm button"
     ).jqBootstrapValidation({
         preventSubmit: true,
-        submitError: function ($form, event, errors) {
+        submitError: function($form, event, errors) {
             // additional error messages or events
         },
-        submitSuccess: function ($form, event) {
+        submitSuccess: function($form, event) {
             event.preventDefault(); // prevent default submit behaviour
             // get values from FORM
             var name = $("input#name").val();
@@ -30,59 +30,59 @@ $(function () {
                     message: message,
                 },
                 cache: false,
-                success: function () {
+                success: function() {
                     // Success message
                     $("#success").html("<div class='alert alert-success'>");
                     $("#success > .alert-success")
                         .html(
-                            "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                            "<button type='button' class='close' data-bs-dismiss='alert' aria-hidden='true'>&times;"
                         )
                         .append("</button>");
                     $("#success > .alert-success").append(
-                        "<strong>Your message has been sent. </strong>"
+                        "<strong>Tu mensaje fue enviado </strong>"
                     );
                     $("#success > .alert-success").append("</div>");
                     //clear all fields
                     $("#contactForm").trigger("reset");
                 },
-                error: function () {
+                error: function() {
                     // Fail message
                     $("#success").html("<div class='alert alert-danger'>");
                     $("#success > .alert-danger")
                         .html(
-                            "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                            "<button type='button' class='close' data-bs-dismiss='alert' aria-hidden='true'>"
                         )
                         .append("</button>");
                     $("#success > .alert-danger").append(
                         $("<strong>").text(
-                            "Sorry " +
-                                firstName +
-                                ", it seems that my mail server is not responding. Please try again later!"
+                            "Lo lamento " +
+                            firstName +
+                            ", ocurrió un error. Vuelve a intentarlo luego!"
                         )
                     );
                     $("#success > .alert-danger").append("</div>");
                     //clear all fields
                     $("#contactForm").trigger("reset");
                 },
-                complete: function () {
-                    setTimeout(function () {
+                complete: function() {
+                    setTimeout(function() {
                         $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
                     }, 1000);
                 },
             });
         },
-        filter: function () {
+        filter: function() {
             return $(this).is(":visible");
         },
     });
 
-    $('a[data-toggle="tab"]').click(function (e) {
+    $('a[data-toggle="tab"]').click(function(e) {
         e.preventDefault();
         $(this).tab("show");
     });
 });
 
 /*When clicking on Full hide fail/success boxes */
-$("#name").focus(function () {
+$("#name").focus(function() {
     $("#success").html("");
 });
